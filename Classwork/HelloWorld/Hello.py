fafafe3874c0627107f9976161da8e06bcb53ce4
Kbratland <116665821+Kbratland@users.noreply.github.com>
2023-08-30 09:37:35 -0700

    1

--- Classwork/HelloWorld/Hello.py
+++ Classwork/HelloWorld/Hello.py
@@ -1,5 +1,14 @@
-print ("Hello World")
+import time
+print ("Hello World, its counting time, there will be 25 amazing unique numbers")
+time.sleep(1)
+print("Get ready to count!")
+time.sleep(1)
 count = 0
-while (count <= 10):
+while (count < 25):
     print ("Count is", count) 
+    time.sleep(0.25)
     count += 1
+if (count == 25):
+    print("count is", count)
+    time.sleep(1)
+    print("Wasn't that fun, we love counting")
