3f395a9b3d6e7c72af278fb04761a6ed41f90a8e
Kbratland <116665821+Kbratland@users.noreply.github.com>
2023-08-30 14:52:44 -0700

    1

--- GUIwork/HelloTkinter.py
+++ GUIwork/HelloTkinter.py
@@ -1,12 +1,23 @@
+import tkinter  as tk
 from tkinter import *
+from tkinter import ttk
 from tkinter.ttk import *
   
-root = Tk()
+root = tk.Tk()
+addto = tk.StringVar()
+root.geometry("500x500")
+root.resizable(False, False)
 root.title = ("HelloTkinter")
-root.geometry =("300x300")
-a = Label(root, text ="Hello World").pack()
-b = Label(root, text = "Why so small").pack()
-button = Button(root, text = 'press me pls')
+a = Label(root, text ="ToDo List").pack()
+listentry = ttk.Entry(root,textvariable=addto).pack()
+button = Button(root, text = 'Enter')
 button.pack(side = TOP, pady = 0)
+button = Button(root, text = 'remove')
+button.pack(side = TOP, pady = 0)
+tasks = ('test', 'test2')
+taskcount = 1
+print(taskcount, "items in list")
+# var = tk.Variable(value=tasks)
+Listbox = tk.Listbox(root, listvariable = tasks, height = taskcount, selectmode=tk.EXTENDED).pack()
   
 root.mainloop()
