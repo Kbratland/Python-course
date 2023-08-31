7087e9a93224d6d25627a77ada9c92a320bf5c08
Kbratland <116665821+Kbratland@users.noreply.github.com>
2023-08-31 10:12:15 -0700

    1

--- Classwork/HelloWorld/Greeter1.py
+++ Classwork/HelloWorld/Greeter1.py
@@ -1,20 +1,30 @@
 
 import time
 
-count = 1
+count = 0
+foodCount = 0
 nameList = []
-while(count <= 3):
+going = True
+while(going == True):
     print("Enter your name")
     nameHolder = input()
     time.sleep(0.15)
+    if(nameHolder == "tally"):
+        print(f"we counted {count} names!")
+        break
+    count += 1
     print("Hello",nameHolder)
     time.sleep(0.15)
     print("Whats your favorite food")
     checker = input()
     time.sleep(0.15)
-    if(checker == "cheese" or checker =="Cheese"):
+    if(checker =="tally"):
+        print(f"we asked for {count} names!")
+        going = False
+        break
+    elif(checker == "cheese" or checker =="Cheese"):
         print("yum")
-    else: print("not cheese :(")
-    count += 1
-    
-print (nameList)
+        foodCount +=1
+    else: 
+        print("not cheese :( but still good")
+        foodCount += 1
---
+++ Classwork/HelloWorld/convoTree.py
@@ -0,0 +1,14 @@
+import time
+
+print("Hello, lets talk about food shall we")
+print("What is you favorite food?")
+if input() == "pizza" or input() ==("pizza"):
+    print("did you know that pizza is the most common favorite food in the world")
+elif input() =="sushi" or input() =="Sushi":
+    print("did you know that sushi is the second most popular food in the world")
+elif input() == "Burger" or input() == "Hamburger" or input() =="Cheeseburger" or input() == "burger" or input() == "hamburger" or input() =="cheeseburger":
+    print("did you know that burgers are the third most common favorite food in the world")
+elif input() == "ramen"or input() =="Ramen":
+    print("Fun fact, Ramen is the fourth most popular food in the world")
+elif input() == "Tacos" or input() ==("tacos"):
+    print("Tacos are the fith most popular food in the world")
