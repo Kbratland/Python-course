9123b865ab077a24d06d2b618323f01ba646412c
Kbratland <116665821+Kbratland@users.noreply.github.com>
2023-09-05 09:41:33 -0700

    123456789

--- Classwork/02_DataTypes/numberMulter.py
+++ Classwork/02_DataTypes/numberMulter.py
@@ -2,13 +2,16 @@ import time
 
 time.sleep(0.25)
 
-print("Hello, please give me number to multiply ")
+print("\nHello, please give me number to multiply (Now accepting floats!)")
 
 time.sleep(0.25)
 
-numVar = int(input("Number: "))
-numVar *= 3
+numVar = float(input("Number: "))
+numMult = float(input("Multiply by:"))
+numResult = round((numVar * numMult),6)
 
 time.sleep(0.15)
 
-print(f"That number multiplied by three is {numVar}")
+print(f"The number {numVar} multiplied by {numMult} is {numResult}\n")
+
+time.sleep(0.25)
