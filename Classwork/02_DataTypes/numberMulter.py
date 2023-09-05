949a7aae82eee808ad58d07fdd331b59427ea8c2
Kbratland <116665821+Kbratland@users.noreply.github.com>
2023-09-05 09:50:06 -0700

    Update numberMulter.py

--- Classwork/02_DataTypes/numberMulter.py
+++ Classwork/02_DataTypes/numberMulter.py
@@ -5,13 +5,20 @@ time.sleep(0.25)
 print("\nHello, please give me number to multiply (Now accepting floats!)")
 
 time.sleep(0.25)
-
-numVar = float(input("Number: "))
-numMult = float(input("Multiply by:"))
+try:
+    numVar = float(input("Number: "))
+except:
+    print("Thats not a number, Goodbye")
+    exit()
+try:
+    numMult = float(input("Multiply by:"))
+except:
+    print("thats not a number, Goodbye")
+    exit
 numResult = round((numVar * numMult),6)
 
 time.sleep(0.15)
 
-print(f"The number {numVar} multiplied by {numMult} is {numResult}\n")
+print(f"The number {numVar} multiplied by {numMult} is {numResult}")
 
 time.sleep(0.25)
