ed366a2416aa6cabdc44179f4e0e2532fec47e5b
Kbratland <116665821+Kbratland@users.noreply.github.com>
2023-09-05 10:06:13 -0700

    123456789

--- Classwork/02_DataTypes/numberMulter.py
+++ Classwork/02_DataTypes/numberMulter.py
@@ -1,24 +1,165 @@
 import time
-
+#-,+,*,/,%,//,**
 time.sleep(0.25)
 
-print("\nHello, please give me number to multiply (Now accepting floats!)")
+print("\nHello, please give me number to work, your current operator is - (Now accepting floats!)")
 
 time.sleep(0.25)
 try:
-    numVar = float(input("Number: "))
+    numVar = float(input("\nNumber: "))
 except:
-    print("Thats not a number, Goodbye")
+    time.sleep(0.25)
+    print("\nThats not a number, Goodbye\n")
     exit()
 try:
-    numMult = float(input("Multiply by:"))
+    numMult = float(input("\nAffect by:"))
 except:
-    print("thats not a number, Goodbye")
-    exit
+    time.sleep(0.25)
+    print("\nthats not a number, Goodbye\n")
+    exit()
+numResult = round((numVar - numMult),6)
+
+time.sleep(0.15)
+
+print(f"\nThe number {numVar} minus {numMult} is {numResult}\n")
+
+time.sleep(0.25)
+
+print("\nHello, please give me number to work, your current operator is + (Now accepting floats!)")
+
+time.sleep(0.25)
+try:
+    numVar = float(input("\nNumber: "))
+except:
+    time.sleep(0.25)
+    print("\nThats not a number, Goodbye\n")
+    exit()
+try:
+    numMult = float(input("\n Affect by:"))
+except:
+    time.sleep(0.25)
+    print("\nthats not a number, Goodbye\n")
+    exit()
+numResult = round((numVar + numMult),6)
+
+time.sleep(0.15)
+
+print(f"\nThe number {numVar} Added to {numMult} is {numResult}\n")
+
+time.sleep(0.25)
+
+print("\nHello, please give me number to work, your current operator is * (Now accepting floats!)")
+
+time.sleep(0.25)
+try:
+    numVar = float(input("\nNumber: "))
+except:
+    time.sleep(0.25)
+    print("\nThats not a number, Goodbye\n")
+    exit()
+try:
+    numMult = float(input("\nAffect by:"))
+except:
+    time.sleep(0.25)
+    print("\nthats not a number, Goodbye\n")
+    exit()
 numResult = round((numVar * numMult),6)
 
 time.sleep(0.15)
 
-print(f"The number {numVar} multiplied by {numMult} is {numResult}")
+print(f"\nThe number {numVar} multiplied by {numMult} is {numResult}\n")
 
 time.sleep(0.25)
+
+print("\nHello, please give me number to work, your current operator is / (Now accepting floats!)")
+
+time.sleep(0.25)
+try:
+    numVar = float(input("\nNumber: "))
+except:
+    time.sleep(0.25)
+    print("\nThats not a number, Goodbye\n")
+    exit()
+try:
+    numMult = float(input("\nAffect by:"))
+except:
+    time.sleep(0.25)
+    print("\nthats not a number, Goodbye\n")
+    exit()
+numResult = round((numVar / numMult),6)
+
+time.sleep(0.15)
+
+print(f"\nThe number {numVar} divided by {numMult} is {numResult}\n")
+
+time.sleep(0.25)
+
+print("\nHello, please give me number to work, your current operator is % (Now accepting floats!)")
+
+time.sleep(0.25)
+try:
+    numVar = float(input("\nNumber: "))
+except:
+    time.sleep(0.25)
+    print("\nThats not a number, Goodbye\n")
+    exit()
+try:
+    numMult = float(input("\nAffect by:"))
+except:
+    time.sleep(0.25)
+    print("\nthats not a number, Goodbye\n")
+    exit()
+numResult = round((numVar % numMult),6)
+
+time.sleep(0.15)
+
+print(f"\nThe number {numVar} is {numResult} percent of {numMult}\n")
+
+time.sleep(0.25)
+
+print("\nHello, please give me number to work, your current operator is // (Now accepting floats!)")
+
+time.sleep(0.25)
+try:
+    numVar = float(input("\nNumber: "))
+except:
+    time.sleep(0.25)
+    print("\nThats not a number, Goodbye\n")
+    exit()
+try:
+    numMult = float(input("\nAffect by:"))
+except:
+    time.sleep(0.25)
+    print("\nthats not a number, Goodbye\n")
+    exit()
+numResult = round((numVar // numMult),6)
+
+time.sleep(0.15)
+
+print(f"\nThe number {numVar} Divided by {numMult} is {numResult}\n")
+
+time.sleep(0.25)
+
+print("\nHello, please give me number to work, your current operator is ** (Now accepting floats!)")
+
+time.sleep(0.25)
+try:
+    numVar = float(input("\nNumber: "))
+except:
+    time.sleep(0.25)
+    print("\nThats not a number, Goodbye\n")
+    exit()
+try:
+    numMult = float(input("\nAffect by:"))
+except:
+    time.sleep(0.25)
+    print("\nthats not a number, Goodbye\n")
+    exit()
+numResult = round((numVar ** numMult),6)
+
+time.sleep(0.15)
+
+print(f"\nThe number {numVar} to the power of {numMult} is {numResult}\n")
+
+time.sleep(0.25)
+
