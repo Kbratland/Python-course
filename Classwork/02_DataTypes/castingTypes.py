72b6e384fcf46a6835cd661cb5349a7e2ad8ee58
Kbratland <116665821+Kbratland@users.noreply.github.com>
2023-09-05 09:26:41 -0700

    Update castingTypes.py

---
+++ Classwork/02_DataTypes/castingTypes.py
@@ -0,0 +1,12 @@
+import time
+
+time.sleep(0.25)
+
+print("Hello, please give me number to multiply ")
+
+numVar = int(input())
+numVar *= 3
+
+time.sleep(0.25)
+
+print(f"That number multiplied by three is {numVar}")
