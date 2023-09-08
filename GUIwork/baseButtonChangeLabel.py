b2d1ae355665b5e5d6aa3f6eecd7ba077a830236
Kbratland <116665821+Kbratland@users.noreply.github.com>
2023-09-08 09:44:57 -0700

    Create baseButtonChangeLabel.py

---
+++ GUIwork/baseButtonChangeLabel.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+root = Tk()
+ 
+text = "new text" 
+
+def changeText():
+    global my_text
+    label.config(text = my_text)
+
+button = Button(root,text = "Please update",command = changeText)
+
+label = Label(root,text = "old text")
+
+label.pack()
+button.pack()
+
+root.mainloop()
