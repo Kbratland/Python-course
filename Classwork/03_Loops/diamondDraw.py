fb62014f81d42b6caed1c58f4d48485e6d8911b2
Kbratland <116665821+Kbratland@users.noreply.github.com>
2023-09-15 09:21:08 -0700

    rename

--- Classwork/03_Loops/diamondDraw.py
+++ Classwork/03_Loops/diamondDraw.py
@@ -4,20 +4,24 @@ import random
 loopCode = True
 diSource = "0"
 diSpace = " "
-randomLowerLetter = chr
+diamondWidth = int(0)
 
 while loopCode == True:
+    
     randomBool = False
     diSpace = " "
     diSource = "0"
     getInput = True
     time.sleep(0.00015)
+    
     while(getInput == True):
+        
         print("\nI am going to draw a diamond, please enter a positive integer (Numbers over 150 will wrap, numbers over 190 will break diamond)\n")
         try:
             diamondWidth = int(input())
             getInput == False
             if diamondWidth <= 0:
+                
                 print("\nThe number has to be above zero, try again\n")
                 continue
             if(diamondWidth %2!=0):
@@ -33,15 +37,22 @@ while loopCode == True:
             
             print("\nThats not a positive even integer, lets try again \n")
             continue
+        
         time.sleep(0.00015)
         print("\nEnter a character to make the diamond out of (Single characters preferred but not necessary)\n")
+        
         try:
+            
             diIn = str(input())
+            
             if diIn.lower() == "random":
+                
                 randomBool = True
                 diSource = "0"
+                
                 break
             else:
+                
                 diSource = diIn
             break
         except:
@@ -54,17 +65,22 @@ while loopCode == True:
 
     level = 1
     widthIn = int(diamondWidth)
+    
     for lp in range(startInt, diamondWidth,2):
+        
         if level <= diamondWidth:
+            
             level += 1
             for lp2 in range(int(diamondWidth - level)):
                 
                 time.sleep(0.00015)
                 if randomBool == True:
+                    
                     randomLowerLetter = chr(random.randint(ord('a'), ord('z')))
                     print(randomLowerLetter,end="")
                 else:
                     print(diSpace,end="")
+                    
             for lp3 in range(int(lp)):
                 
                 time.sleep(0.00015)
@@ -73,10 +89,13 @@ while loopCode == True:
                     print (randomUpperLetter,end="")
                 else:    
                     print(diSource,end="")
+                    
             for lp4 in range(int(diamondWidth - level)):
                 
                 time.sleep(0.00015)
+                
                 if randomBool == True:
+                    
                     randomLowerLetter = chr(random.randint(ord('a'), ord('z')))
                     print(randomLowerLetter,end="")
                 else:
@@ -88,24 +107,33 @@ while loopCode == True:
     diamondWidth -= 1
 
     for lp in range(diamondWidth, 0,-2):
+        
         if level <= diamondWidth:
+            
             level -= 1
             for lp2 in range(int(diamondWidth -level)):
                 
                 time.sleep(0.00015)
                 if randomBool == True:
+                    
                     randomLowerLetter = chr(random.randint(ord('a'), ord('z')))
                     print(randomLowerLetter,end="")
                 else:
+                    
                     print(diSpace,end="")
+                    
             for lp3 in range(int(lp)):
                 
                 time.sleep(0.00015)
+                
                 if randomBool == True:
+                    
                     randomUpperLetter = chr(random.randint(ord('A'), ord('Z')))
                     print (randomUpperLetter,end="")
                 else:    
+                    
                     print(diSource,end="")
+                    
             for lp4 in range(int(diamondWidth - level)):
                 
                 time.sleep(0.00015)
@@ -123,22 +151,30 @@ while loopCode == True:
     print("\n All Done!, Type exit to exit or loop to run again\n")
     randomBool = False
     responding = True
+    
     while responding == True:
+        
         try:
+            
             responseInput = str(input().lower())
+            
             if("exit" in responseInput):
+                
                 print("\nGoodbye\n")
                 loopCode = False
                 break
 
                 
             elif("loop" in responseInput):
+                
                 print("\nOk, Looping\n")
                 responding = False
                 break
             else:
+                
                 print("\nThats not a response I can handle, lets try that again\n")
                 continue
         except:
+            
             print("\nThats not a response I can handle, lets try that again\n")
             continue
