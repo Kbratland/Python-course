2d4a15edeae37ecf59cb0410fcfcfd21ce187176
Kbratland <116665821+Kbratland@users.noreply.github.com>
2023-09-25 09:49:33 -0700

    Update listIntro.py

--- Classwork/04_Lists/listIntro.py
+++ Classwork/04_Lists/listIntro.py
@@ -1,6 +1,13 @@
-names = ["wesly", "Cheese", "gorgonzola"]
+names = ["wesley", "Cheese", "gorgonzola"]
+# print(names)
+# names.append("nameable ")
+# print(names)
+# print(names.pop(1))
+# print(names)
+# del names[0]
+# print(names)
+names.insert(1, "bOriginal")
+names.insert(1, "b")
 print(names)
-names.append("nameadded")
-print(names)
-print(names.pop(1))
+del names[2]
 print(names)
