7af7d6c42d19dbc4695ea7fcaef0aa6e35cf73a3
Kbratland <116665821+Kbratland@users.noreply.github.com>
2023-09-25 10:01:01 -0700

    Update loopList.py

--- Classwork/04_Lists/loopList.py
+++ Classwork/04_Lists/loopList.py
@@ -1,3 +1,12 @@
-temps = [122,121,214,532,65,3,53]
-for n in range(5):
+print("")
+
+temps = [122, 121, 214, 532, 121, 23, 352453, 543, 23423432, 542, 65, 3, 53]
+for n in range(len(temps)):
     print(temps[n])
+    
+print("")
+
+for p in temps:
+    print(p)
+
+print("")
