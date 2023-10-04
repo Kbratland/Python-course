a02602a6fe4581bf70ff690f0814ee974ef0002c
Kbratland <116665821+Kbratland@users.noreply.github.com>
2023-10-04 09:51:38 -0700

    Update selectSortList.py

--- Classwork/04_Lists/selectSortList.py
+++ Classwork/04_Lists/selectSortList.py
@@ -3,7 +3,7 @@ import random
 numList = []
 
 
-def listGen(listIn,amount):
+def listGen(listIn, amount):
     for lp in range(amount):
         listIn.append(random.randint(0, 11))
         lp += 1
@@ -25,57 +25,53 @@ def minNum(listIn):
         lp += 1
     return int(lowestNum)
 
+
 def sortList(listIn):
     tempList = []
     tempLen = len(listIn)
     lp = 0
     while lp < tempLen:
-        tempNum = minNum(listIn)         
+        tempNum = minNum(listIn)
         listIn.remove(tempNum)
         tempList.append(tempNum)
         lp += 1
     return tempList
 
 
-def quickSort(listIn,lowIndex,highIndex,pivotIndex):
+def quickSort(listIn, lowIndex, highIndex, pivotIndex):
     pivot = pivotIndex
     lowIn = lowIndex
     highIn = highIndex
-    print(f"\nStart high is {highIn} (value is {listIn[highIn]}), start low is {lowIn} (value is {listIn[lowIn]}), start pivot is {pivot} (value is {listIn[pivot]})")
-    print(listIn,"\n")
     goingUp = True
     swap = True
-    while swap:
-        print(f"\nHigh is {highIn} (value is {listIn[highIn]}),  low is {lowIn} (value is {listIn[lowIn]}),  pivot is {pivot} (value is {listIn[pivot]})")
-        print("\n",listIn)
+    while swap and highIn > lowIn:
         if goingUp:
-            
             if listIn[pivot] > listIn[highIn]:
-                print("changing direction")
-                (listIn[pivot],listIn[highIn]) = (listIn[highIn],listIn[pivot])
-                if highIn == pivot:
-                    swap = False
+                (listIn[pivot], listIn[highIn]) = (
+                    listIn[highIn], listIn[pivot])
                 pivot = highIn
-                lowIn +=1
+                lowIn += 1
                 goingUp = False
             else:
                 highIn -= 1
         else:
-            
+
             if listIn[pivot] < listIn[lowIn]:
-                print("changing direction")
-                (listIn[pivot],listIn[lowIn]) = (listIn[lowIn],listIn[pivot])
-                if lowIn == pivot:
-                    swap = False
+                (listIn[pivot], listIn[lowIn]) = (listIn[lowIn], listIn[pivot])
                 pivot = lowIn
-                highIn -=1
+                highIn -= 1
                 goingUp = True
             else:
-                lowIn +=1
-    print(f"\nEnd high is {highIn} (value is {listIn[highIn]}), end low is {lowIn} (value is {listIn[lowIn]}), end pivot is {pivot} (value is {listIn[pivot]})")
-    print(listIn)
+                lowIn += 1
+    print(listIn, "\n")
+    if pivot > lowIndex:
+        quickSort(listIn, 0,pivot-1,0)
+    if pivot < highIndex:
+        quickSort(listIn,pivot+1,highIndex,pivot+1)
 
-listGen(numList,25)
-print(quickSort(numList,0,len(numList)-1,0))
+
+listGen(numList, 25)
+print(numList)
+quickSort(numList, 0, len(numList)-1, 0)
 # print("\nOriginal List:",numList)
 # print("\nThe sorted list is:",sortList(numList))
