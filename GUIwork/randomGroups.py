c9f9f22b1a96a95125be017a0be81f96e2066236
Kbratland <116665821+Kbratland@users.noreply.github.com>
2023-10-18 09:06:49 -0700

    update

--- GUIwork/randomGroups.py
+++ GUIwork/randomGroups.py
@@ -59,10 +59,10 @@ def quitList(listBin, list2):
     listFile = open("listFile")
     tempList = []
     temp2 = []
-    for lp in range(num):
+    for lp in range(num + 1):
         tempList.append(listBin.get(lp))
         temp2.append(list2.get(lp))
-    num = len(tempList)-1
+    num = len(tempList)
     listFile["groupHold"] = temp2
     listFile["numHold"] = num
     listFile["listHold"] = tempList
@@ -71,8 +71,6 @@ def quitList(listBin, list2):
 
 
 def loadList(listBin, list2):
-
-    
 
     global num
     try:
@@ -91,89 +89,60 @@ def loadList(listBin, list2):
 
 def randomize(listIn, listOut, amount, mode):
 
-    global num #num is amount of people in the list
+    global num  # num is amount of people in the list
     listOut.delete(0, END)
     tempList = []
     for lp in range(num):
         tempList.append(listIn.get(lp))
-
     if mode == 1:
-
-        print("\n", "----------------------------------------execute randomize by name----------------------------------------")
-
-        print("\n", f"amount is {amount}")
-
+        print("\n", "execute randomize by name")
+        # print("\n", f"amount is {amount}")
         allowedNum = []
-
-        for lp in range(len(tempList)):#I took out the minus 2
+        for lp in range(len(tempList)):  # I took out the minus 2
             allowedNum.append(lp)
-       
-        print("\n", f"length of allowed list is {len(allowedNum)}")
-
+        # print("\n", f"length of allowed list is {len(allowedNum)}")
         if (len(tempList)) % amount != 0:
             groupAmount = ceil((len(tempList))/(amount))
-
-            print("\n", f"Group amount is {groupAmount} (ceilinged)")
-
+            # print("\n", f"Group amount is {groupAmount} (ceilinged)")
         else:
             groupAmount = int((len(tempList))/amount)
-
-            print("\n", f"Group amount is {groupAmount} (Divisible)")
-
-        for curSpot in range(amount): #loops through the groups
-            for lp in range(groupAmount): #choose a member of the group randomly
-
-                print("\n", f"Current spot is {curSpot}")
+            # print("\n", f"Group amount is {groupAmount} (Divisible)")
+        for curSpot in range(amount):  # loops through the groups
+            for lp in range(groupAmount):  # choose a member of the group randomly
+                # print("\n", f"Current spot is {curSpot}")
                 if (len(allowedNum) <= 1):
                     tNum = 0
                 else:
                     tNum = randint(0, len(allowedNum) - 1)
-                
-                print("\n", f"tNum is {tNum}")
-
+                # print("\n", f"tNum is {tNum}")
                 try:
-
                     randIn = allowedNum[tNum]
-
-                    print(
-                        "\n", f"The random index is {randIn} out of {allowedNum}")
-
+                    # print(
+                    # "\n", f"The random index is {randIn} out of {allowedNum}")
                     tempList[randIn] += f" is in group {curSpot+1}"
-
-                    print("\n", f"the new entry is {tempList[randIn]}")
-
+                    # print("\n", f"the new entry is {tempList[randIn]}")
                     del allowedNum[tNum]
                 except Exception as e:
-
-                    print("\n", f"excepted, error is {e}")
-                    print(allowedNum)
+                    # print("\n", f"excepted, error is {e}")
+                    # print(allowedNum)
                     pass
-
-        print("\n", "Outing")
-        print(tempList)
+        # print("\n", "Outing")
+        # print(tempList)
         for lp in range(len(tempList)):
             listOut.insert(lp, tempList[lp])
-
     else:
-
         print("\n", "execute randomize by number")
-
         temp2 = []
         holdInt = len(tempList)
         for main in range(amount):
             for lp in range(ceil(holdInt/amount)):
-
                 print("\n", ceil(holdInt/amount))
-
                 try:
                     wordIn = tempList.pop(randint(0, len(tempList)-1))
                 except:
                     break
                 if wordIn != "":
                     temp2.append(wordIn + f" is in group {main+1}")
-
-                print("\n", tempList)
-
             main += 1
     if mode != 1:
         for lp in range(0, len(temp2)):
@@ -188,26 +157,26 @@ name_label = Label(root, text=" Add a student's name ",
                    font=('calibre', 20, 'bold'))
 
 name_entry = Entry(root, textvariable=name_var, font=(
-    'calibre', 12, 'normal'), bg="black")
+    'calibre', 12, 'normal'))
 num_label = Label(root, text='Edit Name:', font=('calibre', 20, 'bold'))
 
 num_entry = Entry(root, textvariable=num_var, font=(
-    'calibre', 12, 'normal'), bg="black")
+    'calibre', 12, 'normal'))
 
 sub_btn = Button(root, text='Enter', command=lambda: submit(1))
 
 sub_btn2 = Button(root, text='Remove Selected', command=delete)
 
 sub_btn4 = Button(root, text="Group by name", command=lambda: randomize(
-    listbox, list_2, int(ginVar.get()), mode=1,debug=True))
+    listbox, list_2, int(ginVar.get()), mode=1))
 sub_btn5 = Button(root, text="Group by number", command=lambda: randomize(
-    listbox, list_2, int(ginVar.get()), mode=2,debug=False))
+    listbox, list_2, int(ginVar.get()), mode=2))
 
 sub_btn3 = Button(root, text="Edit", command=changeEntry)
 
 groups_label = Label(root, text="Enter the amount of groups")
 groups_entry = Entry(root, textvariable=ginVar, font=(
-    'calibre', 12, 'normal'), bg="black")
+    'calibre', 12, 'normal'))
 g2_label = Label(root, text='Groups:', font=('calibre', 15, 'bold'))
 n1_label = Label(root, text='Names:', font=('calibre', 15, 'bold'))
 
