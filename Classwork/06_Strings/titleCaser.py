fc42097e90505f11da128c2acaa9f14801f2f7eb
Kbratland <116665821+Kbratland@users.noreply.github.com>
2023-10-26 10:02:24 -0700

    Update titleCaser.py

--- Classwork/06_Strings/titleCaser.py
+++ Classwork/06_Strings/titleCaser.py
@@ -1,22 +1,25 @@
 from pyperclip import *
 
+
 def titleCase():
     returnString = ""
-    noCap = ['a', 'and', 'as', 'at', 'but', 'by', 'down', 'for', 'from', 'if', 'in', 'into', 'like', 'near', 'nor', 'of', 'off', 'on', 'once', 'onto', 'or', 'over', 'past', 'so', 'than', 'that', 'to', 'upon', 'when', 'with', 'Yet']
+    noCap = ['a', 'and', 'as', 'at', 'but', 'by', 'down', 'for', 'from', 'if', 'in', 'into', 'like', 'near', 'nor',
+             'of', 'off', 'on', 'once', 'onto', 'or', 'over', 'past', 'so', 'than', 'that', 'to', 'upon', 'when', 'with', 'Yet']
     stringList = []
     stringList += paste().split()
     for lp in range(len(stringList)):
         stringList[lp] = stringList[lp].lower()
-    for lp in range(0,len(stringList)):
-        if(lp == 0):
+    for lp in range(0, len(stringList)):
+        if (lp == 0):
             stringList[lp] = stringList[lp].capitalize()
-        elif(lp == len(stringList)-1):
-           stringList[lp] = stringList[lp].capitalize()
+        elif (lp == len(stringList)-1):
+            stringList[lp] = stringList[lp].capitalize()
         else:
             if not stringList[lp] in noCap:
                 stringList[lp] = stringList[lp].capitalize()
         returnString += stringList[lp] + " "
     copy(returnString)
     return returnString
+
 
 print(titleCase())
