5427d40d254755725a8c605d2f4dbe84ddb10d8e
Kbratland <116665821+Kbratland@users.noreply.github.com>
2023-10-26 10:07:09 -0700

    Update titleCaser.py

--- Classwork/06_Strings/titleCaser.py
+++ Classwork/06_Strings/titleCaser.py
@@ -6,9 +6,7 @@ def titleCase():
     noCap = ['a', 'and', 'as', 'at', 'but', 'by', 'down', 'for', 'from', 'if', 'in', 'into', 'like', 'near', 'nor',
              'of', 'off', 'on', 'once', 'onto', 'or', 'over', 'past', 'so', 'than', 'that', 'to', 'upon', 'when', 'with', 'Yet']
     stringList = []
-    stringList += paste().split()
-    for lp in range(len(stringList)):
-        stringList[lp] = stringList[lp].lower()
+    stringList += paste().lower().split()
     for lp in range(0, len(stringList)):
         if (lp == 0 or lp == len(stringList)-1):
             stringList[lp] = stringList[lp].capitalize()
