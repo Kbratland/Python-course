92fd937cd0064963bce725eec667fa408c60cd54
Kbratland <116665821+Kbratland@users.noreply.github.com>
2023-10-30 10:06:53 -0700

    Update regex.py

--- Classwork/06_Strings/regex.py
+++ Classwork/06_Strings/regex.py
@@ -1,6 +1,6 @@
 from re import *
 text = "this is a phone number at +1(503)-718-1287"
 regComp = compile(
-    r'''(\+(\d|\d{2}|\d{3}))?(\d{3}|\(\d{3}\))?(\s|-|\.)?(\d{3})(\s|-|\.)?(\d{4})''')
+    r'''(\+\d)?(\d{3}|\(\d{3}\))?(\s|-|\.)?(\d{3})(\s|-|\.)?(\d{4})''')
 matches = regComp.search(text)
-print(matches.group())
+print(matches.group(0))
