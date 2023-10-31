44b0836909a600064a972bbb0549c70cf3f15fc9
Kbratland <116665821+Kbratland@users.noreply.github.com>
2023-10-31 09:50:43 -0700

    Update regex.py

--- Classwork/06_Strings/regex.py
+++ Classwork/06_Strings/regex.py
@@ -1,7 +1,7 @@
 from re import *
 
-text = "this is a phone number at +1(503)-718-1287"
+text = "this is a phone number at +1(503)-718-1287, i can also be reached at 971-221-1283 or (942).357.4318"
 regComp = compile(
     r'''(\+(?:\d)*)?(\d{3}|\(\d{3}\))?(\s|-|\.)?(\d{3})(\s|-|\.)?(\d{4})''')
-matches = regComp.search(text)
-print(matches.group())
+matches = regComp.findall(text)
+print(matches)
