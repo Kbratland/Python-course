20b3cf4001ea6d083ade7e04b5a5a252c750e977
Kbratland <116665821+Kbratland@users.noreply.github.com>
2023-11-01 09:44:54 -0700

    purchase lsit

--- Classwork/06_Strings/buyThings.py
+++ Classwork/06_Strings/buyThings.py
@@ -1,16 +1,17 @@
 from re import *
 from pyperclip import *
 
-def makeShopList(text):
+def makeShopList(text = "I want 9 jars of pickles, 7 cheeses,  I could use about 4 waffles, and 3 beans, maybe also pickup 82 fish."):
     listString = []
-    buyReg = compile(r"\d+\s\w+")
+    outText = ""
+    buyReg = compile(r"\d+\D+[?.\,|\.]")
     for groups in buyReg.findall(text):
-        listString.append("\n"+groups) 
-    return listString
+        listString.append("\n"+groups)
+    for lp in range(len(listString)):
+        listString[lp] = listString[lp][:len(listString[lp])-1] 
+    outText = " ".join(listString)
+    return outText
 
+print(makeShopList(paste()))
 
-temp = ("I want 9 pickles 7 cheeses,  I could use about 4 waffles and 3 beans, maybe also pickup 82 fish")
-listTemp = (makeShopList(paste()))
-for lp in range(len(listTemp)):
-    print(listTemp[lp])
     
