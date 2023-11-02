3e5fef6c44b210224a17c36c2fddf07d596e337c
Kbratland <116665821+Kbratland@users.noreply.github.com>
2023-11-02 10:09:32 -0700

    Update requestDemo.py

--- Classwork/07_WebScraping/requestDemo.py
+++ Classwork/07_WebScraping/requestDemo.py
@@ -1,9 +1,9 @@
 from requests import *
 
-result = get("https://mclainonline.com")
+result = get("https://mclainonline.com/")
 try:
     result.raise_for_status()
 except Exception as e:
     print(f"Error retrieving webpage: {e}")
     exit()
-print("Yippee")
+print("result.text")
