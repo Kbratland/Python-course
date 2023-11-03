60755468c1da2cf30e7ad0bd416404e0f7704cab
Kbratland <116665821+Kbratland@users.noreply.github.com>
2023-11-03 10:09:54 -0700

    E

---
+++ Classwork/07_WebScraping/WeatherRequester.py
@@ -0,0 +1,21 @@
+from requests import *
+from bs4 import *
+from pyperclip import *
+
+result = get("https://www.wunderground.com/weather/us/or/portland/KORPORTL605")
+
+try:
+    result.raise_for_status()
+except Exception as e:
+    print(f"Error retrieving webpage: {e}")
+    exit()
+    
+newSoup = BeautifulSoup(result.text,'html.parser')
+highFind = newSoup.select(".hi-lo .hi")
+lowFind = newSoup.select(".hi-lo .lo")
+currentFind = newSoup.select(".current-temp")
+feelsFind = newSoup.select(".feels-like")
+print("The high for today is " + highFind[0].getText())
+print("The low for today is " + lowFind[0].getText())
+print("The current temperature is " + currentFind[0].getText())
+print("Feels" + feelsFind[0].getText())
--- Classwork/07_WebScraping/requestDemo.py
+++ Classwork/07_WebScraping/requestDemo.py
@@ -1,4 +1,6 @@
+from webbrowser import *
 from requests import *
+from bs4 import *
 
 result = get("https://riverdaleschool.com/")
 try:
@@ -6,4 +8,13 @@ try:
 except Exception as e:
     print(f"Error retrieving webpage: {e}")
     exit()
-print(result.text)
+    
+newSoup = BeautifulSoup(result.text,'html.parser')
+newsElement = newSoup.select('.home-news .fsTitle')
+linkElement = newSoup.select('.home-news .fsTitle a')
+#Get titles
+for headline in newsElement:
+    print(headline.getText().strip() + " - opening!")
+for link in linkElement:
+    open(link.attrs["href"])
+#Get links and addresses to open tabs with each
