fa5a367fe553834ed8ba098d64c6a88846823787
Kbratland <116665821+Kbratland@users.noreply.github.com>
2023-11-14 10:10:02 -0800

    e

--- image/imgConvert.py
+++ image/imgConvert.py
@@ -2,11 +2,29 @@ from PIL import Image
 
 ascii_characters_by_surface = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 
+ds = False
+menu = True
+while menu:
+    inp = input("For discord? Y/N ")
+    if inp.lower() == "n":
+        ds = False
+        menu=False
+        print("\n Converting.... \n")
+    elif inp.lower() == "y":
+        ds = True
+        menu=False
+        print("\n Converting.... \n")
+    else:
+        print("Not acceptable, try again\n")
+        continue
 
 def main():
     image = Image.open('image.jpg')
     # you can first resize the image if needed
-    image = image.resize((60,34))
+    if ds:
+        image = image.resize((60, 33))
+    else:
+        image = image.resize((1920, 1080))
     ascii_art = convert_to_ascii_art(image)
     save_as_text(ascii_art)
 
@@ -35,7 +53,7 @@ def convert_pixel_to_character(pixel):
 def save_as_text(ascii_art):
     with open("image.txt", "w") as file:
         for line in ascii_art:
-            # print(line)
+            print(line)
             file.write(line)
             file.write('\n')
         file.close()
