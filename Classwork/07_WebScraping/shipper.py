a0f1959e361366cb0345b3077b6a5f173d41fc44
Konner Bratland <116665821+Kbratland@users.noreply.github.com>
2024-09-10 12:25:00 -0700

    Summary (required)

--- Classwork/07_WebScraping/shipper.py
+++ Classwork/07_WebScraping/shipper.py
@@ -2,9 +2,9 @@ from webbrowser import *
 from random import *
 
 searchString = ""
-for lp in range(15):
+for lp in range(25):
     searchString = ""
-    for lp in range(0,5):
+    for lp in range(0,randint(1,5)):
         searchString +=  chr(randint(ord('a'), ord('z')))
         searchString +=  chr(randint(ord('A'), ord('Z')))
         searchString += str(randint(0,99))
